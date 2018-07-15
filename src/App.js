fdf55e93e8abc8006af5c8cf7b8fba5bffe37752
kirandeep <rana.kirandeep@gmail.com>
2018-07-15 21:35:24 +1000

    Code formatting

--- src/App.js
+++ src/App.js
@@ -7,70 +7,79 @@ import './App.css';
 
 class App extends Component {
 
-  constructor(){
-    super();
-  this.state = {
-    projects:[
-      {
-    title: "project1",
-    category: "web devlopment"
+    constructor() {
+        super();
+        this.state = {
+            projects: [{
+                    title: "project1",
+                    category: "web devlopment"
 
-  },{
-    title: "project2",
-    category: "app devlopment"
+                }, {
+                    title: "project2",
+                    category: "app devlopment"
 
-  },{
-    title: "project3",
-    category: "mobile devlopment"
+                }, {
+                    title: "project3",
+                    category: "mobile devlopment"
 
-  }
-  
-    ]
-  } 
-}
+                }
 
-addProject(newProject){
-  
-let projects= this.state.projects;
-projects.push(newProject);
+            ]
+        }
+    }
 
-this.setState({projects
+    addProject(newProject) {
 
-})
+        let projects = this.state.projects;
+        projects.push(newProject);
 
-}
+        this.setState({
+            projects
 
-onDelete(title){
+        })
 
-let projects=this.state.projects;
+    }
 
-let index=projects.findIndex(x=> x.title===title);
+    onDelete(title) {
 
-console.log(index);
+        let projects = this.state.projects;
 
-projects.splice(index,1);
+        let index = projects.findIndex(x => x.title === title);
 
-this.setState({projects})
+        console.log(index);
 
-}
+        projects.splice(index, 1);
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <AddProject addProject={this.addProject.bind(this)}/>
-        <hr />
-        <Project test="hello world" projects={this.state.projects}  onDelete={this.onDelete.bind(this)} />
-        
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+        this.setState({ projects })
+
+    }
+
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            header className = "App-header" >
+            <
+            img src = { logo } className = "App-logo"
+            alt = "logo" / >
+            <
+            h1 className = "App-title" > Welcome to React < /h1> <
+            /header> <
+            AddProject addProject = { this.addProject.bind(this) }
+            /> <
+            hr / >
+            <
+            Project test = "hello world"
+            projects = { this.state.projects } onDelete = { this.onDelete.bind(this) }
+            />
+
+            <
+            p className = "App-intro" >
+            To get started, edit < code > src / App.js < /code> and save to reload. <
+            /p> <
+            /div>
+        );
+    }
 }
 
 export default App;
